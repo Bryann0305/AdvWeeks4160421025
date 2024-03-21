8689a9e13f1990ef9dd754ab4decd77c5e6b9178
Bryann0305 <s160421025@student.ubaya.ac.id>
2024-03-21 09:58:13 +0700

    project

---
+++ src/main/java/com/ubaya/advweek4nrp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.advweek4nrp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.advweek4nrp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
